5aeeeba77b9e13592137722e1dd471310500f2c8
Paulo Rodrigo <paulorodrigo1992@gmail.com>
2021-11-10 00:23:06 -0300

    Hadling exceptions and clearing screen

--- untitled/src/application/UI.java
+++ untitled/src/application/UI.java
@@ -4,6 +4,7 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
 
+import java.sql.SQLOutput;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -30,6 +31,10 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static void clearScreen(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
     public static ChessPosition readChessPosition(Scanner sc){
         try {
             String s = sc.nextLine();
